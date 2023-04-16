2ffa5057f7cb91eee9f85560a90f9d8cf6304431
mostafashraf18 <ma2963629@gmail.com>
2023-04-16 13:34:06 +0200

    first commit

---
+++ include/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <signal.h>
+#pragma comment(lib, "ws2_32.lib")
+
+
+
+#endif
---
+++ src/server.c
@@ -0,0 +1,59 @@
+#include "../include/server.h"
+#include <stdio.h>
+
+#define BACKLOG 10
+
+
+int main() {
+
+  WSADATA wsaData;
+
+ if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+ fprintf(stderr, "WSAStartup failed.\n");
+ exit(1); }
+ if (LOBYTE(wsaData.wVersion) != 2 ||
+ HIBYTE(wsaData.wVersion) != 2) {
+ fprintf(stderr,"Versiion 2.2 of Winsock is not available.\n");
+ WSACleanup();
+ exit(2);
+  }
+
+  
+  FILE *html_data;
+  html_data = fopen("index.html", "r");
+
+  char response[1024];
+  fgets(response, 1024, html_data);
+
+  char http_header[2048] = "HTTP/1.1 200 OK\r\n\n";
+  strcat(http_header, response);
+
+  int server_socket;
+  server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  
+
+  struct sockaddr_in server_address;
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(8001);
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  
+
+  
+  
+  bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+
+  listen(server_socket, BACKLOG);
+  int client_socket;
+  while(1){
+    
+    client_socket = accept(server_socket, NULL, NULL);
+    send(client_socket, http_header, sizeof(http_header), 0);
+    close(client_socket);
+  }
+  
+  
+  
+  return 0;
+  
+
+}
